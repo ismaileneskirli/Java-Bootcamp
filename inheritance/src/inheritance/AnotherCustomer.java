1277d526b9dd9ae446c5f7e4e3783e235cb09aa6
ismaileneskirli <69651222+ismaileneskirli@users.noreply.github.com>
2021-05-01 02:21:08 +0300

    eclipse push

---
+++ inheritance/src/inheritance/AnotherCustomer.java
@@ -0,0 +1,7 @@
+package inheritance;
+
+public class AnotherCustomer extends Customer {
+	
+	String anotherThingy;
+
+}
--- inheritance/src/inheritance/Main.java
+++ inheritance/src/inheritance/Main.java
@@ -6,12 +6,16 @@ public class Main {
 		// TODO Auto-generated method stub
 
 		
-		
+		// polymorphism exercise.
 		IndividualCustomer Enes = new IndividualCustomer();
 		Enes.customerNumber = "12312321";
 		
 		CustomerManager customerManager = new CustomerManager();
 		customerManager.add(Enes);
+		
+		AnotherCustomer anotherCustomer = new AnotherCustomer();
+		anotherCustomer.customerNumber = "1123414";
+		customerManager.add(anotherCustomer);
 	}
 
 }
