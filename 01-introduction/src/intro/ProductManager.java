98c5f52896569d60076f3f9081c8bbb3fb701a4a
ismaileneskirli <69651222+ismaileneskirli@users.noreply.github.com>
2021-05-02 02:12:48 +0300

    ordered the progress

---
+++ 01-introduction/src/intro/ProductManager.java
@@ -0,0 +1,9 @@
+package intro;
+
+public class ProductManager {
+	
+	public void addToCart(Product product) {
+		System.out.println("Following product added to cart: " + product.getName());
+	}
+
+}
